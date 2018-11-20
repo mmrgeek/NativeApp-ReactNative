bef19ef51f6d59cf45d69b01a615dc033adc7f16
Mostafa Mohamed <mostafa11.mm6@gmail.com>
2018-11-20 19:30:48 +0200

    another commit

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -25,6 +25,7 @@ export default class App extends Component {
             value: ""
         };
         this.handleOnTextChange = this.handleOnTextChange.bind(this);
+        this.handleOnTextChangePassword = this.handleOnTextChangePassword.bind(this);
     }
 
     handleOnTextChange(data){
@@ -33,15 +34,55 @@ export default class App extends Component {
         });
     }
 
+    handleOnTextChangePassword(data){
+        this.setState({
+            value: data
+        });
+    }
+
+    pressLogin(){
+
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <TextInput
-                    defaultValue={this.state.defaultValue} 
-                    onChangeText={this.handleOnTextChange}/>
-                <Text>
-                    {this.state.value}
-                </Text>
+
+                <View style={{flex:1 ,justifyContent:'center',flexDirection:'row'}}>
+                    <Text 
+                        style={{color :"#37474f", fontSize: 100,alignContent:'center',marginTop:50}}>
+                        Login
+                    </Text>
+                </View>
+                <View style={{flex:2,marginTop:100}}>
+                    <Text style={styles.Tstyle}>Username</Text>
+                    <TextInput
+                        placeholder=""
+                        style={styles.te_EmlStyle}
+                        underlineColorAndroid="#62727b"
+                        onChangeText={this.handleOnTextChange}/>
+
+                    <Text style={styles.Tstyle}>Password</Text> 
+
+                    <TextInput 
+                        placeholder=""
+                        style={styles.te_PassStyle}
+                        underlineColorAndroid="#62727b"
+                        onChange={this.handleOnTextChangePassword}/>
+
+                    <Button 
+                            style={{
+                                marginTop:10
+                            
+                                ,marginRight:10
+                                ,marginLeft:10
+                                ,marginBottom:10
+                                ,width:200 
+                                ,height:100   }}
+                            color="#0d47a1" 
+                            title= "Login" 
+                            onPress={this.pressLogin}/>
+                </View>
             </View>
         );
     }
@@ -49,6 +90,26 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: '#fff',
         flex: 1,
+        justifyContent: 'center',
+    },
+    te_PassStyle: {
+        marginRight: 15,
+        marginLeft: 15 ,
+        fontSize:18,
+    },
+    te_EmlStyle:{
+        marginRight: 15,
+        marginLeft: 15,
+        fontSize:18,
+    },
+    Tstyle: {
+        fontSize: 18,
+        color:'#37474f',
+        marginLeft: 15,
+        marginRight:15,
+        marginBottom: 15,
+        
     },
 });
